75746587f3691d24cf7c0219ac9d6235c5f64347
Koollboy <starsever62@gmail.com>
2023-12-23 23:04:35 -0800

    synch checking

--- app/src/main/java/com/example/invoicemaker/activity/ViewPDFPreviewActivity.java
+++ app/src/main/java/com/example/invoicemaker/activity/ViewPDFPreviewActivity.java
@@ -8,6 +8,8 @@ import com.example.invoicemaker.R;
 
 public class ViewPDFPreviewActivity extends AppCompatActivity {
 
+    // ok
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
